8a57c038fe7904a3d25f3b1014543ce87d877132
xzotacx <jesus.152006@gmail.com>
2021-06-01 09:53:07 -0400

    Cambiando imagenes a webp

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Caroudel from "../Carousel";
 
-import image1 from "../../assets/images/Image-1.jpg";
-import image2 from "../../assets/images/Image-2.jpg";
-import image3 from "../../assets/images/Image-3.jpg";
+import image1 from "../../assets/images/Image-1.webp";
+import image2 from "../../assets/images/Image-2.webp";
+import image3 from "../../assets/images/Image-3.webp";
 
 function Header() {
   return (
